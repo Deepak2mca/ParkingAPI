a9cd94bc5576a2d5a758658852f7a11eb00317a4
Deepak Sahu <sahu.deepak01@gmail.com>
2018-03-16 14:24:40 +0530

    ParkingAPI

---
+++ LocationController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MongoDB.Bson;
+using MongoDB.Driver;
+using Newtonsoft.Json;
+using ParkingService.Models;
+
+namespace ParkingService.Controllers
+{
+    [Route("api/[controller]")]
+    public class LocationController : Controller
+    {
+
+        [HttpGet]
+        public string Get()
+        {           
+            List<Location> aList = MongoDBHelper.GetEntityList<Location>();
+            var json = JsonConvert.SerializeObject(aList);
+            return json;
+        }
+        [HttpGet]
+        public string GetByLocationName(string locname)
+        {
+            List<Location> location = MongoDBHelper.GetCollection<Location>().FindAllAs<Location>().Where<Location>(sb => sb.name == locname).ToList<Location>();
+            var json = JsonConvert.SerializeObject(location);
+            return json;
+        }
+        [HttpGet]
+        public string GetByLatLong(Loc loc)
+        {            
+            List<Location> location = MongoDBHelper.GetCollection<Location>().FindAllAs<Location>().Where<Location>(sb => sb.loc.coordinates[0] == loc.coordinates[0] && sb.loc.coordinates[1] == loc.coordinates[1]).ToList<Location>();
+            var json = JsonConvert.SerializeObject(location);
+            return json;
+        }
+        [HttpPut]
+        public int AddLocation()
+        {
+            System.Collections.Generic.List<double> items2 = new System.Collections.Generic.List<double> { 1.23222, 4.56222 };
+            Location documnt = new Location
+            {
+                _id = MongoDB.Bson.ObjectId.GenerateNewId().ToString(),
+                name = "Parking Spaces,Dadar",
+                tspaces = 200,
+                aspaces = 70,
+                loc = new Loc() { type = "point", coordinates = items2 }
+            };
+            MongoDBHelper.InsertEntity<Location>(documnt);
+            return 1;
+        }
+    }
+}
---
+++ MongoDBHelper.cs
@@ -0,0 +1,47 @@
+﻿using MongoDB.Driver;
+using System.Collections.Generic;
+using System.Linq;
+using MongoDB.Driver.Linq;
+
+namespace ParkingService.Models
+{
+    public static class MongoDBHelper
+    {
+        private static string connectionString = "mongodb://deepak:M0ngo@XXX.YY.ZZZ.AAA/db01";
+        public static string DatabaseName { get { return "db02"; } }       
+        private static MongoDatabase _database;
+        public static MongoDatabase DB
+        {
+            get
+            {
+                if (_database == null)
+                {
+                    //var Client = new MongoClient(connectionString);
+                    MongoClient client = new MongoClient(connectionString);
+                    MongoServer server = client.GetServer();
+                    _database = server.GetDatabase(DatabaseName);                   
+                    return _database;
+                }
+                return _database;
+            }
+        }
+
+       
+
+        public static MongoCollection<T> GetCollection<T>() where T : Entity
+        {
+            return DB.GetCollection<T>(typeof(T).Name.ToLower());
+        }
+
+        public static List<T> GetEntityList<T>() where T : Entity
+        {           
+            var collection = DB.GetCollection<T>(typeof(T).Name.ToLower());            
+            return collection.AsQueryable().ToList<T>();
+        }
+
+        public static void InsertEntity<T>(T entity) where T : Entity
+        {
+            GetCollection<T>().Save(entity);
+        }
+    }
+}
